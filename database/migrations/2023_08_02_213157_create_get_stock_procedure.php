f01e404d71f33a4ca17ca82b4c66f3065a0660d3
alejandro-cn-dev <alejandro.cn.1996@gmail.com>
2023-08-03 00:02:59 -0400

    fix(DB): add test SP's #1

---
+++ database/migrations/2023_08_02_213157_create_get_stock_procedure.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $procedimiento = "CREATE PROCEDURE `get_stock_by_productid`(IN `idx` INT, OUT `stock` INT) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN SET stock = (SELECT COALESCE(SUM(cantidad),0) FROM `compra_detalles` WHERE id_producto = idx AND isDeleted = 0) - (SELECT COALESCE(SUM(cantidad),0) FROM `venta_detalles` WHERE id_producto = idx AND isDeleted = 0); END";
+        DB::unprepared($procedimiento);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $drop_procedimiento = " DROP PROCEDURE IF EXISTS `get_stock_by_productid`";
+        DB::unprepared($drop_procedimiento);
+    }
+};
---
+++ database/migrations/2023_08_02_223818_create_get_inventario_procedure.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $procedimiento = "CREATE PROCEDURE `get_inventario_by_productid` () 
+        BEGIN 
+        SELECT a.id, a.item_producto, a.descripcion, b.detalle AS categoria, d.detalle AS marca, a.color, a.unidad, a.precio_compra, a.precio_venta, ((SELECT COALESCE(SUM(cantidad),0) FROM compra_detalles WHERE id_producto = a.id) - (SELECT COALESCE(SUM(cantidad),0) FROM venta_detalles WHERE id_producto = a.id)) AS existencias FROM `productos` AS a INNER JOIN `categorias` AS b ON b.id = a.id_categoria INNER JOIN `almacens` AS c ON c.id = a.id_almacen INNER JOIN `marcas` AS d ON d.id = a.id_marca WHERE a.isDeleted = 0; END";
+        DB::unprepared($procedimiento);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $drop_procedimiento = "DROP PROCEDURE `get_inventario_by_productid`;";
+        DB::unprepared($drop_procedimiento);
+    }
+};
