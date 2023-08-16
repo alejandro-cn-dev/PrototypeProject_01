40b261f297f495b2b3e3423d074999151d174a61
alejandro-cn-dev <alejandro.cn.1996@gmail.com>
2023-08-15 23:54:04 -0400

    fix(ui): change card information home dashboard #2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Venta_cabecera;
+use App\Models\Venta_detalle;
 use App\Models\Compra_cabecera;
+use App\Models\Compra_detalle;
 use App\Models\User;
 use App\Models\Producto;
+use App\Models\Proveedor;
+use App\Models\Cliente;
 use App\Models\Rol;
 
 class HomeController extends Controller
@@ -32,10 +36,32 @@ class HomeController extends Controller
         $compras = Compra_cabecera::where('isDeleted','=',0)->count();
         $empleados = User::where('isDeleted','=',0)->count();
         $productos = Producto::where('isDeleted','=',0)->count();
+        $existencia_adquirida = Compra_detalle::join('productos','compra_detalles.id_producto','=','productos.id')        
+        ->join('categorias','productos.id_categoria','=','categorias.id')
+        ->where('compra_detalles.isDeleted','=',0)
+        ->where('productos.isDeleted','=',0)
+        ->where('categorias.nombre','NOT LIKE','%Hilo%')
+        ->sum('compra_detalles.cantidad');
+        $existencia_vendida = Venta_detalle::join('productos','venta_detalles.id_producto','=','productos.id')        
+        ->join('categorias','productos.id_categoria','=','categorias.id')
+        ->where('venta_detalles.isDeleted','=',0)
+        ->where('productos.isDeleted','=',0)
+        ->where('categorias.nombre','NOT LIKE','%Hilo%')
+        ->sum('venta_detalles.cantidad');
+        $proveedores = Proveedor::where('isDeleted','=',0)->count();
+        $clientes = Cliente::count();
+        $total_compras = Compra_cabecera::where('isDeleted','=',0)->sum('monto_total');
+        $total_ventas = Venta_cabecera::where('isDeleted','=',0)->sum('monto_total');
         return view('home')
         ->with('ventas',$ventas)
         ->with('compras',$compras)
         ->with('empleados',$empleados)
-        ->with('productos',$productos);
+        ->with('productos',$productos)
+        ->with('existencia_adq',$existencia_adquirida)
+        ->with('existencia_ven',$existencia_vendida)
+        ->with('proveedores',$proveedores)
+        ->with('clientes',$clientes)
+        ->with('total_compras',$total_compras)
+        ->with('total_ventas',$total_ventas);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,31 +11,94 @@
 
 @section('content')
     <!-- PRIMERA SECCION -->
+    <!-- Fila 0 de admin -->
+    @can('admin-dashboard')
+    <div class="row" bis_skin_checked="1">
+        <!-- Tarjeta #1 ADM -->
+        <div class="col-lg-3 col-6" bis_skin_checked="1">
+          <!-- small box -->
+          <div class="small-box bg-success" bis_skin_checked="1">
+            <div class="inner" bis_skin_checked="1">
+              <h3>{{$empleados}}</h3>
+              <p>Usuarios registrados</p>
+            </div>
+            <div class="icon" bis_skin_checked="1">
+              <i class="fa fa-user-plus" aria-hidden="true"></i>
+            </div>
+            <a href="/empleados" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div> 
+        <!-- Tarjeta #2 ADM -->        
+        <div class="col-lg-3 col-6" bis_skin_checked="1">
+          <!-- small box -->
+          <div class="small-box bg-primary" bis_skin_checked="1">
+            <div class="inner" bis_skin_checked="1">
+              <h3>{{$total_compras}} Bs.</h3>
+              <p>Costos de adquisición</p>
+            </div>
+            <div class="icon" bis_skin_checked="1">
+              <i class="fas fa-fw fa-box" aria-hidden="true"></i>
+            </div>
+            <a href="/compras" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>      
+        <!-- Tarjeta #3 ADM -->        
+        <div class="col-lg-3 col-6" bis_skin_checked="1">
+          <!-- small box -->
+          <div class="small-box bg-warning" bis_skin_checked="1">
+            <div class="inner" bis_skin_checked="1">
+              <h3>{{$total_ventas}} Bs.</h3>
+              <p>Ingresos por ventas</p>
+            </div>
+            <div class="icon" bis_skin_checked="1">
+              <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+            </div>
+            <a href="/ventas" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>   
+        <!-- Tarjeta #4 ADM -->
+        <div class="col-lg-3 col-6" bis_skin_checked="1">
+          <!-- small box -->
+          <div class="small-box bg-danger" bis_skin_checked="1">
+            <div class="inner" bis_skin_checked="1">
+              <h3>291 Bs.</h3>
+              <p>Ganancia</p>
+            </div>
+            <div class="icon" bis_skin_checked="1">
+              <i class="fa fa-usd" aria-hidden="true"></i>
+            </div>
+            <a href="#" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div> 
+    </div>
+    @endcan
+    <!-- FIN de Fila 0 de admin -->
+    <!-- Primera fila -->
     <div class="row" bis_skin_checked="1">
         <!-- Tarjeta #1 -->
         <div class="col-lg-3 col-6" bis_skin_checked="1">
           <!-- small box -->
-          <div class="small-box bg-success" bis_skin_checked="1">
+          <div class="small-box bg-olive" bis_skin_checked="1">
             <div class="inner" bis_skin_checked="1">
               <h3>{{ $compras }}</h3>
               <p>Compras</p>
             </div>
             <div class="icon" bis_skin_checked="1">
-              <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+              <i class="fas fa-fw fa-box" aria-hidden="true"></i>
             </div>
-            <a href="#" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="/compras" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>    
         <!-- Tarjeta #2 -->
         <div class="col-lg-3 col-6" bis_skin_checked="1">
           <!-- small box -->
-          <div class="small-box bg-info" bis_skin_checked="1">
+          <div class="small-box bg-warning" bis_skin_checked="1">
             <div class="inner" bis_skin_checked="1">
               <h3>{{$ventas}}</h3>
               <p>Total de Ventas</p>
             </div>
             <div class="icon" bis_skin_checked="1">
-              <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+              <i class="fa fa-shopping-cart" aria-hidden="true"></i>
             </div>
             <a href="/ventas" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
           </div>
@@ -44,7 +107,7 @@
         <!-- Tarjeta #3 -->
         <div class="col-lg-3 col-6" bis_skin_checked="1">
           <!-- small box -->
-          <div class="small-box bg-warning" bis_skin_checked="1">
+          <div class="small-box bg-purple" bis_skin_checked="1">
             <div class="inner" bis_skin_checked="1">
               <h3>{{$productos}}</h3>
               <p>Productos registrados</p>
@@ -58,77 +121,81 @@
         <!-- Tarjeta #4 -->
         <div class="col-lg-3 col-6" bis_skin_checked="1">
           <!-- small box -->
-          <div class="small-box bg-primary" bis_skin_checked="1">
+          <div class="small-box bg-gray" bis_skin_checked="1">
             <div class="inner" bis_skin_checked="1">
-              <!-- <h3>74<sup style="font-size: 20px">%</sup></h3> -->
-              <h3>74</h3>
-              <p>Existencia actual (Metros de tela)</p>
-            </div>
-            <div class="icon" bis_skin_checked="1">
-              <i class="fa fa-signal" aria-hidden="true"></i>
-            </div>
-            <a href="#" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
-          </div>
-        </div>        
-    </div>
-    <div class="row" bis_skin_checked="1">
-        <!-- Tarjeta #5 -->
-        <div class="col-lg-3 col-6" bis_skin_checked="1">
-          <!-- small box -->
-          <div class="small-box bg-secondary" bis_skin_checked="1">
-            <div class="inner" bis_skin_checked="1">
-              <h3>1</h3>
+              <h3>{{$proveedores}}</h3>
               <p>Proveedores</p>
             </div>
             <div class="icon" bis_skin_checked="1">
               <i class="fa fa-user" aria-hidden="true"></i>
             </div>
-            <a href="/ventas" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="/proveedores" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
           </div>
-        </div>
-        <!-- Tarjeta #6 -->
+        </div>  
+    </div>
+    <!-- FIN de Primera fila -->
+    <!-- Segunda fila -->
+    <div class="row" bis_skin_checked="1">
+        <!-- Tarjeta #5 -->
         <div class="col-lg-3 col-6" bis_skin_checked="1">
           <!-- small box -->
-          <div class="small-box bg-warning" bis_skin_checked="1">
+          <div class="small-box bg-info" bis_skin_checked="1">
             <div class="inner" bis_skin_checked="1">
-              <h3>{{$empleados}}</h3>
-              <p>Usuarios registrados</p>
+              <!-- <h3>74<sup style="font-size: 20px">%</sup></h3> -->
+              <h3>{{ $existencia_adq }}</h3>
+              <p>Metros de tela adquirida</p>
             </div>
             <div class="icon" bis_skin_checked="1">
-              <i class="fa fa-user-plus" aria-hidden="true"></i>
+              <i class="fas fa-fw fa-box" aria-hidden="true"></i>
             </div>
-            <a href="/empleados" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="#" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>        
+        <!-- Tarjeta #6 -->        
+        <div class="col-lg-3 col-6" bis_skin_checked="1">
+          <!-- small box -->
+          <div class="small-box bg-fuchsia" bis_skin_checked="1">
+            <div class="inner" bis_skin_checked="1">
+              <h3>{{$existencia_ven}}</h3>
+              <p>Metros de tela vendida</p>
+            </div>
+            <div class="icon" bis_skin_checked="1">
+              <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+            </div>
+            <a href="#" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>               
         <!-- Tarjeta #7 -->        
         <div class="col-lg-3 col-6" bis_skin_checked="1">
           <!-- small box -->
-          <div class="small-box bg-success" bis_skin_checked="1">
+          <div class="small-box bg-primary" bis_skin_checked="1">
             <div class="inner" bis_skin_checked="1">
-              <h3>80.00 Bs.</h3>
-              <p>Ganacias</p>
+              <h3>{{$existencia_adq - $existencia_ven}}</h3>
+              <p>Metros de tela en existencia</p>
             </div>
             <div class="icon" bis_skin_checked="1">
               <i class="fas fa-fw fa-check" aria-hidden="true"></i>
             </div>
-            <a href="/productos" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="#" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>        
         <!-- Tarjeta #8 -->
         <div class="col-lg-3 col-6" bis_skin_checked="1">
           <!-- small box -->
-          <div class="small-box bg-danger" bis_skin_checked="1">
+          <div class="small-box bg-orange" bis_skin_checked="1">
             <div class="inner" bis_skin_checked="1">
-              <h3>10</h3>
-              <p>Existencia vendida (metros de tela)</p>
+              <h3>{{$clientes}}</h3>
+              <p>Clientes registrados</p>
             </div>
             <div class="icon" bis_skin_checked="1">
-              <i class="fa fa-signal" aria-hidden="true"></i>
+              <i class="fa fa-user-plus" aria-hidden="true"></i>
             </div>
-            <a href="/empleados" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="#" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
           </div>
-        </div>
+        </div> 
     </div>
+    <!-- FIN de Segunda Fila -->
+    
       <!-- FIN PRIMERA SECCION -->
 
       <!-- SEGUNDA SECCION -->
