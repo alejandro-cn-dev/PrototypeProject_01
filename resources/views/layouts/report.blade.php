9eca77cc6705809aa790e505337b62eeaded08a3
alejandro-cn-dev <alejandro.cn.1996@gmail.com>
2023-11-22 00:00:20 -0400

    feat(reports): add report layout & fix report icon

--- resources/views/layouts/report.blade.php
+++ resources/views/layouts/report.blade.php
@@ -1,57 +1,80 @@
 <!doctype html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>@yield('title')</title>
-<style type="text/css">
-    * {
-        font-family: Verdana, Arial, sans-serif;
-    }
-    table{
-        font-size: 12px;
-    }
-    .total{
-        font-weight: bold;
-        font-size: 12px;
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    <style type="text/css">
+        * {
+            font-family: Verdana, Arial, sans-serif;
+        }
+        header, footer{
+            background-color: lavender;
+        border-radius: 8px;
+        }
+        main{
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+        footer{
+            font-size: 10px;
+            display: flex;
+            justify-content: space-between;
+            padding: 10px;
+        }
+        table{
+            font-size: 12px;
+            border: 1px solid #CCC;
+            border-collapse: collapse;
+        }
+        td{
+        border: none;
+        }
+        .total{
+            font-weight: bold;
+            font-size: 12px;
+            border: 1px solid black;
+        }
+        .gray {
+            background-color: lightgray
+        }
+        #contenido{
+        border-collapse:collapse;
+        }
+        #contenido thead tr th{
         border: 1px solid black;
-    }
-    .gray {
-        background-color: lightgray
-    }
-    #contenido{
-	border-collapse:collapse;
-    }
-    #contenido thead tr th{
-	border: 1px solid black;
-    }
-    #contenido tbody tr th{
-	border: 1px solid black;
-    }
-    #contenido tbody tr td{
-	border: 1px solid black;
-    }
-</style>
-
+        }
+        #contenido tbody tr th{
+        border: 1px solid black;
+        }
+        #contenido tbody tr td{
+        border: 1px solid black;
+        }
+    </style>
 </head>
 <body>
-
-  <table width="100%">
-    <tr>
-        <!-- <td valign="top"><img src="{{public_path().'\\img\\logo.jpg'}}" alt="" width="150"/></td> -->
-        <td valign="top"><img src="{{ public_path('img/logo.jpg') }}" alt="" width="150"/></td>
-
-        <td align="left">
-            <h2>Empresa Comercial "Presitex"</h2>
-	      </td>
-        <td align="right">
-          <h3>Lugar y fecha:</h3>
-          <pre>La Paz, @yield('fecha')</pre>
-        </td>
-    </tr>
-
-  </table>
-  <h3 align="center">@yield('cabecera')</h3>
-  <hr size="3" color="black" />
-  @yield('content')  
+    <header>
+        <table style="border: none; width: 100%">
+            <tr>        
+                <td valign="top" style="vertical-align: middle;	text-align-last: center; ">
+                    <img src="{{ public_path('img/logo_report.jpg') }}" alt="" style="width: 75px;"/>
+                </td>
+                <td align="left">
+                <h2>@yield('empresa')</h2>
+            </td>
+                <td align="right">
+                <h3>Lugar y fecha:</h3>
+                <pre>La Paz, @yield('fecha')</pre>
+                </td>
+            </tr>
+        </table>
+    </header>
+    <main>
+        <h3 align="center">@yield('cabecera')</h3>
+        @yield('content')  
+    </main>
+    <footer>
+        <p> <b>Dirección:</b> Calle Isaac Tamayo, Galería "Centro Comercial Isaac Tamayo", 1er Piso (Local 103 - 104), La Paz.</p>
+        <p><b>Teléfono:</b> (+591) 719-96-788</p>
+    </footer>
 </body>
 </html>
--- resources/views/marca/pdf_marca.blade.php
+++ resources/views/marca/pdf_marca.blade.php
@@ -1,56 +1,13 @@
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Listado de Marcas</title>
-<style type="text/css">
-    * {
-        font-family: Verdana, Arial, sans-serif;
-    }
-    table{
-        font-size: 12px;
-    }
-    .total{
-        font-weight: bold;
-        font-size: 12px;
-        border: 1px solid black;
-    }
-    .gray {
-        background-color: lightgray
-    }
-    #contenido{
-	border-collapse:collapse;
-    }
-    #contenido thead tr th{
-	border: 1px solid black;
-    }
-    #contenido tbody tr th{
-	border: 1px solid black;
-    }
-    #contenido tbody tr td{
-	border: 1px solid black;
-    }
-</style>
-
-</head>
-<body>
-
-  <table width="100%">
-    <tr>
-        <td valign="top"><img src="{{ public_path('img/logo.jpg') }}" alt="" width="150"/></td>
-
-        <td align="left">
-            <h2>Tienda Textil "Presitex"</h2>
-	      </td>
-        <td align="right">
-          <h3>Lugar y fecha:</h3>
-          <pre>La Paz, {{$fecha}}</pre>
-        </td>
-    </tr>
-
-  </table>
-  <h3 align="center">Listado de Marcas</h3>
-  <hr size="3" color="black" />
+@extends('layouts.report')
+@section('tittle', 'Marcas')
+@section('empresa')
+  Empresa Comercial "Presitex"
+@stop
+@section('fecha')
+  {{$fecha}}
+@stop
+@section('cabecera','Listado de marcas')
+@section('content')
   <table id="contenido" width="100%" >
     <thead style="background-color: lightgray;">
       <tr>
@@ -59,13 +16,13 @@
       </tr>
     </thead>
     <tbody>
-      @foreach($marcas as $marca)
+    @foreach($marcas as $marca)
       <tr>
         <th scope="row">{{$marca->id}}</th>
         <td>{{$marca->detalle}}</td>
       </tr>
-      @endforeach
+    @endforeach
     </tbody>  
   </table>
-</body>
-</html>
+  
+@stop
