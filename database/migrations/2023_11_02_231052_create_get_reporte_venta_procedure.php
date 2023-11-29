c9ae474a406420c2ee83d3f54b1d5308e3b9ee04
alejandro-cn-dev <alejandro.cn.1996@gmail.com>
2023-11-28 23:45:03 -0400

    fix(DB): fix SP 'get_reporte_venta_by_arg'

--- database/migrations/2023_11_02_231052_create_get_reporte_venta_procedure.php
+++ database/migrations/2023_11_02_231052_create_get_reporte_venta_procedure.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -14,11 +15,14 @@ return new class extends Migration
     public function up()
     {
         $procedimiento = "DROP PROCEDURE IF EXISTS `get_reporte_venta_by_arg`;
-        CREATE PROCEDURE `get_reporte_venta_by_arg`(IN `ARG` VARCHAR(25)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER 
+        CREATE PROCEDURE `get_reporte_venta_by_arg`(IN `ARG` VARCHAR(25)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER
         BEGIN
             DECLARE fecha_hoy DATE DEFAULT '2023-01-01';
             DECLARE fecha DATE;
             SET fecha_hoy = CURDATE();
+            IF ARG = 'all' THEN
+            	SET fecha = DATE_SUB(fecha_hoy, INTERVAL 10 YEAR);
+            END IF;
             IF ARG = 'hoy' THEN
                 SET fecha = fecha_hoy;
             END IF;
@@ -28,8 +32,8 @@ return new class extends Migration
             IF ARG = 'mes' THEN
                 SET fecha = DATE_SUB(fecha_hoy, INTERVAL 1 MONTH);
             END IF;
-            
-            SELECT productos.nombre, productos.item_producto, venta_detalles.precio_unitario, SUM(venta_detalles.cantidad) AS ventas_totales, (venta_detalles.precio_unitario * (SUM(venta_detalles.cantidad))) AS total FROM `venta_detalles` JOIN `venta_cabeceras` ON `venta_detalles`.`id_venta` = `venta_cabeceras`.`id` JOIN `productos` ON `venta_detalles`.`id_producto` = `productos`.`id` WHERE `venta_cabeceras`.`fecha_venta` >= fecha AND `venta_detalles`.`isDeleted` = 0 GROUP BY `productos`.`nombre`,`productos`.`item_producto`; 
+
+            SELECT productos.nombre, productos.item_producto, venta_detalles.precio_unitario, SUM(venta_detalles.cantidad) AS ventas_totales, (venta_detalles.precio_unitario * (SUM(venta_detalles.cantidad))) AS total FROM `venta_detalles` JOIN `venta_cabeceras` ON `venta_detalles`.`id_venta` = `venta_cabeceras`.`id` JOIN `productos` ON `venta_detalles`.`id_producto` = `productos`.`id` WHERE `venta_cabeceras`.`fecha_venta` >= fecha AND `venta_detalles`.`isDeleted` = 0 GROUP BY `productos`.`nombre`,`productos`.`item_producto`;
         END;";
         DB::unprepared($procedimiento);
     }
