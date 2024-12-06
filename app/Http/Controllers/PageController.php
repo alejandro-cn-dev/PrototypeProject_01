aceeb95fe0ab57778c27095a8371ac84f5065b84
alejandro-cn-dev <alejandro.cn.1996@gmail.com>
2024-12-06 11:11:45 -0400

    fixed home page backend & UI

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -21,11 +21,11 @@ class PageController extends Controller
     // Vista principal de la homepage
     public function index()
     {
-        $productos = Producto::join('marcas','productos.id_marca','=','marcas.id')
-        ->join('categorias','productos.id_categoria','=','categorias.id')
-        ->select('productos.id','productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','marcas.detalle as marca','categorias.nombre as categoria')
-        ->where('productos.isDeleted','=',0)
-        ->get();
+        // $productos = Producto::join('marcas','productos.id_marca','=','marcas.id')
+        // ->join('categorias','productos.id_categoria','=','categorias.id')
+        // ->select('productos.id','productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','marcas.detalle as marca','categorias.nombre as categoria')
+        // ->where('productos.isDeleted','=',0)
+        // ->get();
         $productos = DB::table('productos')
         ->leftjoin('venta_detalles', 'productos.id', '=', 'venta_detalles.id_producto')
         ->join('marcas','productos.id_marca','=','marcas.id')
@@ -33,7 +33,8 @@ class PageController extends Controller
         ->leftjoin('compra_detalles','productos.id', '=', 'compra_detalles.id_producto')
         ->select(
             'productos.id','productos.nombre','productos.color','productos.precio_venta','productos.unidad','marcas.detalle as marca','categorias.nombre as categoria',
-            DB::raw('(COALESCE(SUM(compra_detalles.cantidad), 0) - COALESCE(SUM(venta_detalles.cantidad), 0)) AS existencia'),
+            //DB::raw('(COALESCE(SUM(compra_detalles.cantidad), 0) - COALESCE(SUM(venta_detalles.cantidad), 0)) AS existencia'),
+            DB::raw('(COALESCE(SUM(CASE WHEN compra_detalles.isDeleted = 0 THEN compra_detalles.cantidad ELSE 0 END), 0) - COALESCE(SUM(CASE WHEN venta_detalles.isDeleted = 0 THEN venta_detalles.cantidad ELSE 0 END), 0)) AS existencia'),
         )
         ->groupBy('productos.id','productos.nombre','productos.color','productos.precio_venta','productos.unidad','marcas.detalle','categorias.nombre')
         ->orderByDesc('existencia')
@@ -58,10 +59,16 @@ class PageController extends Controller
     // Deberia poder filtrar con un buscador aqui
     public function lista()
     {
-        $productos = Producto::join('marcas','productos.id_marca','=','marcas.id')
+        $productos = DB::table('productos')
+        ->select(
+            'productos.id','productos.item_producto','productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','marcas.detalle as marca','productos.id_categoria','categorias.nombre as categoria',
+            DB::raw('(COALESCE(SUM(CASE WHEN compra_detalles.isDeleted = 0 THEN compra_detalles.cantidad ELSE 0 END), 0) - COALESCE(SUM(CASE WHEN venta_detalles.isDeleted = 0 THEN venta_detalles.cantidad ELSE 0 END), 0)) AS existencia'),
+        )
+        ->leftjoin('compra_detalles','productos.id', '=', 'compra_detalles.id_producto')
+        ->leftjoin('venta_detalles', 'productos.id', '=', 'venta_detalles.id_producto')
         ->join('categorias','productos.id_categoria','=','categorias.id')
-        // ->select('productos.id','productos.descripcion','productos.color','productos.existencia','productos.precio_venta','productos.unidad_venta','marcas.detalle as marca','categorias.nombre as categoria')
-        ->select('productos.id','productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','marcas.detalle as marca','categorias.nombre as categoria')
+        ->join('marcas','productos.id_marca','=','marcas.id')
+        ->groupBy('productos.id','productos.item_producto','productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','marca','productos.id_categoria','categoria')
         ->where('productos.isDeleted','=',0)
         ->get();
         $imagenes = Imagen::where('tabla','=','productos')->get();
@@ -79,31 +86,71 @@ class PageController extends Controller
     {
         // Recuperar registro de producto mediante 'id'
         //$producto = Producto::find($id);
-        $producto = Producto::join('marcas','productos.id_marca','=','marcas.id')
+        // $producto = Producto::join('marcas','productos.id_marca','=','marcas.id')
+        // ->join('categorias','productos.id_categoria','=','categorias.id')
+        // ->select('productos.id','productos.id_categoria','productos.item_producto',DB::raw("((SELECT COALESCE(SUM(cantidad),0) FROM `compra_detalles` WHERE id_producto = productos.id AND isDeleted = 0) - (SELECT COALESCE(SUM(cantidad),0) FROM `venta_detalles` WHERE id_producto = productos.id AND isDeleted = 0)) AS existencia"),'productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','marcas.detalle as marca','categorias.nombre as categoria')
+        // ->where('productos.isDeleted','=',0)
+        // ->where('productos.id','=',$id)
+        // ->first();
+        $producto_detalle = DB::table('productos')
+        ->select(
+            'productos.id','productos.item_producto','productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','productos.material','productos.calidad','marcas.detalle as marca','productos.id_categoria','categorias.nombre as categoria',
+            DB::raw('(COALESCE(SUM(CASE WHEN compra_detalles.isDeleted = 0 THEN compra_detalles.cantidad ELSE 0 END), 0) - COALESCE(SUM(CASE WHEN venta_detalles.isDeleted = 0 THEN venta_detalles.cantidad ELSE 0 END), 0)) AS existencia'),
+            //DB::raw('(COALESCE(SUM(compra_detalles.cantidad), 0) - COALESCE(SUM(venta_detalles.cantidad), 0)) AS existencia'),
+        )
+        ->leftjoin('compra_detalles','productos.id', '=', 'compra_detalles.id_producto')
+        ->leftjoin('venta_detalles', 'productos.id', '=', 'venta_detalles.id_producto')
         ->join('categorias','productos.id_categoria','=','categorias.id')
-        ->select('productos.id','productos.id_categoria','productos.item_producto',DB::raw("((SELECT COALESCE(SUM(cantidad),0) FROM `compra_detalles` WHERE id_producto = productos.id AND isDeleted = 0) - (SELECT COALESCE(SUM(cantidad),0) FROM `venta_detalles` WHERE id_producto = productos.id AND isDeleted = 0)) AS existencia"),'productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','marcas.detalle as marca','categorias.nombre as categoria')
+        ->join('marcas','productos.id_marca','=','marcas.id')
+        ->groupBy('productos.id','productos.item_producto','productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','productos.material','productos.calidad','marca','productos.id_categoria','categoria')
         ->where('productos.isDeleted','=',0)
         ->where('productos.id','=',$id)
         ->first();
         // Recuperar registros de productos que son de la misma categoria por 'id_categoria'
-        $relacionados = Producto::join('marcas','productos.id_marca','=','marcas.id')
+        // $relacionados = Producto::join('marcas','productos.id_marca','=','marcas.id')
+        // ->join('categorias','productos.id_categoria','=','categorias.id')
+        // ->select('productos.id','productos.nombre','productos.color','productos.precio_venta','productos.unidad','marcas.detalle as marca','categorias.nombre as categoria')
+        // ->where('productos.isDeleted','=',0)
+        // ->where('productos.id_categoria','=',$producto->id_categoria)
+        // ->where('productos.id','!=',$id)
+        // ->get();
+        $relacionados = DB::table('productos')
+        ->select(
+            'productos.id','productos.item_producto','productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','marcas.detalle as marca','productos.id_categoria','categorias.nombre as categoria',
+            //DB::raw('(COALESCE(SUM(CASE WHEN compra_detalles.isDeleted = 0 THEN compra_detalles.cantidad ELSE 0 END), 0) - COALESCE(SUM(CASE WHEN venta_detalles.isDeleted = 0 THEN venta_detalles.cantidad ELSE 0 END), 0)) AS existencia'),
+            DB::raw('(COALESCE(SUM(compra_detalles.cantidad), 0) - COALESCE(SUM(venta_detalles.cantidad), 0)) AS existencia'),
+        )
+        ->leftjoin('compra_detalles','productos.id', '=', 'compra_detalles.id_producto')
+        ->leftjoin('venta_detalles', 'productos.id', '=', 'venta_detalles.id_producto')
         ->join('categorias','productos.id_categoria','=','categorias.id')
-        ->select('productos.id','productos.nombre','productos.color','productos.precio_venta','productos.unidad','marcas.detalle as marca','categorias.nombre as categoria')
+        ->join('marcas','productos.id_marca','=','marcas.id')
+        ->groupBy('productos.id','productos.item_producto','productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.id_categoria','productos.unidad','marcas.detalle','categorias.nombre')
+        ->orderByDesc('existencia')
         ->where('productos.isDeleted','=',0)
-        ->where('productos.id_categoria','=',$producto->id_categoria)
+        ->where('productos.id_categoria','=',$producto_detalle->id_categoria)
         ->where('productos.id','!=',$id)
+        ->limit(4)
         ->get();
         $imagenes = Imagen::where('tabla','=','productos')->where('id_registro','=',$id)->first();
         $imagenes_rel = Imagen::where('tabla','=','productos')->get();
-        return view('vitrina.product', ['producto' => $producto, 'relacionados' => $relacionados, 'imagenes' => $imagenes, 'imagenes_rel' => $imagenes_rel]);
+        return view('vitrina.product', ['producto' => $producto_detalle, 'relacionados' => $relacionados, 'imagenes' => $imagenes, 'imagenes_rel' => $imagenes_rel]);
     }
 
     // Buscar productos
     public function buscar(Request $request){
         $product =$request->get('product');
-        $result = Producto::join('marcas','productos.id_marca','=','marcas.id')
+        $result = DB::table('productos')
+        ->select(
+            'productos.id','productos.item_producto','productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','marcas.detalle as marca','productos.id_categoria','categorias.nombre as categoria',
+            DB::raw('(COALESCE(SUM(CASE WHEN compra_detalles.isDeleted = 0 THEN compra_detalles.cantidad ELSE 0 END), 0) - COALESCE(SUM(CASE WHEN venta_detalles.isDeleted = 0 THEN venta_detalles.cantidad ELSE 0 END), 0)) AS existencia'),
+            //DB::raw('(COALESCE(SUM(compra_detalles.cantidad), 0) - COALESCE(SUM(venta_detalles.cantidad), 0)) AS existencia'),
+        )
+        ->leftjoin('compra_detalles','productos.id', '=', 'compra_detalles.id_producto')
+        ->leftjoin('venta_detalles', 'productos.id', '=', 'venta_detalles.id_producto')
         ->join('categorias','productos.id_categoria','=','categorias.id')
-        ->select('productos.id','productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','marcas.detalle as marca','categorias.nombre as categoria')
+        ->join('marcas','productos.id_marca','=','marcas.id')
+        ->groupBy('productos.id','productos.item_producto','productos.nombre','productos.descripcion','productos.color','productos.precio_venta','productos.unidad','marca','productos.id_categoria','categoria')
+        ->where('productos.isDeleted','=',0)
         ->where('productos.nombre','LIKE','%'.$product.'%')
         ->orWhere('productos.item_producto','LIKE','%'.$product.'%')->get();
         $imagenes = Imagen::where('tabla','=','productos')->get();
--- resources/views/vitrina/product.blade.php
+++ resources/views/vitrina/product.blade.php
@@ -8,31 +8,33 @@
         <div class="container px-4 px-lg-5 my-5">
             <div class="row gx-4 gx-lg-5 align-items-center">
                 <div class="col-md-6">
-                    @if(empty($imagenes))
-                        <img class="card-img-top mb-5 mb-md-0" src="{{ asset('img/product_generic_img_3.jpg') }}" alt="{{$producto->nombre}} imagen" />
+                    @if (empty($imagenes))
+                        <img class="card-img-top mb-5 mb-md-0" src="{{ asset('img/product_generic_img_3.jpg') }}"
+                            alt="{{ $producto->nombre }} imagen" />
                     @else
-                        <img class="card-img-top mb-5 mb-md-0" src="{{ asset('storage/img/'.$imagenes->nombre_imagen) }}" alt="{{$producto->nombre}} imagen" />
-                    @endif                    
+                        <img class="card-img-top mb-5 mb-md-0" src="{{ asset('storage/img/' . $imagenes->nombre_imagen) }}"
+                            alt="{{ $producto->nombre }} imagen" />
+                    @endif
                 </div>
                 <div class="col-md-6">
-                    <!-- Notificacion de existencias -->                    
-                        <!-- Notifications <span class="badge badge-light">4</span> -->
-                        @if ($producto->existencia == 0)
-                            <!-- <button type="button" class="btn btn-danger">Agotado</button> -->
-                            <div class="alert alert-danger" role="alert">
-                                Agotado
-                            </div>
-                        @elseif ($producto->existencia < 5)
-                            <!-- <button type="button" class="btn btn-warning">Por agotarse</button> -->
-                            <div class="alert alert-warning" role="alert">
-                                Por agotarse
-                            </div>
-                        @else
-                            <!-- <button type="button" class="btn btn-success">Disponible</button> -->
-                            <div class="alert alert-success" role="alert">
-                                Disponible
-                            </div>
-                        @endif                    
+                    <!-- Notificacion de existencias -->
+                    <!-- Notifications <span class="badge badge-light">4</span> -->
+                    @if ($producto->existencia == 0)
+                        <!-- <button type="button" class="btn btn-danger">Agotado</button> -->
+                        <div class="alert alert-danger" role="alert">
+                            Agotado
+                        </div>
+                    @elseif ($producto->existencia < 5)
+                        <!-- <button type="button" class="btn btn-warning">Por agotarse</button> -->
+                        <div class="alert alert-warning" role="alert">
+                            Por agotarse
+                        </div>
+                    @else
+                        <!-- <button type="button" class="btn btn-success">Disponible</button> -->
+                        <div class="alert alert-success" role="alert">
+                            Disponible
+                        </div>
+                    @endif
                     <div class="small mb-1">ITEM: {{ $producto->item_producto }}</div>
                     <h1 class="display-5 fw-bolder">{{ $producto->nombre }}</h1>
                     <div class="fs-5 mb-5">
@@ -41,26 +43,29 @@
                     </div>
                     <p class="lead">{{ $producto->descripcion }}</p>
                     <p class="lead">
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item"><b>Marca: </b>{{$producto->marca}}</li>
-                            <li class="list-group-item"><b>Categoria: </b>{{$producto->categoria}}</li>
-                            <li class="list-group-item"><b>Color: </b>{{$producto->color}}</li>
-                            @if($producto->existencia > 0)
-                            <li class="list-group-item"><b>Existencias: </b>{{$producto->existencia}} @if($producto->unidad_venta === 'unidad') unidad(es) @elseif($producto->unidad_venta === 'rollo') rollo(s) @else metro(s)@endif</li>
-                            @endif                            
-                        </ul>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><b>Marca: </b>{{ $producto->marca }}</li>
+                        <li class="list-group-item"><b>Categoria: </b>{{ $producto->categoria }}</li>
+                        <li class="list-group-item"><b>Color: </b>{{ $producto->color }}</li>
+                        <li class="list-group-item"><b>Calidad: </b>{{ $producto->calidad }}</li>
+                        <li class="list-group-item"><b>Material: </b>{{ $producto->material }}</li>
+                        @if ($producto->existencia > 0)
+                            <li class="list-group-item"><b>Existencias:
+                                </b>{{ $producto->existencia . ' ' . $producto->unidad . '(s)' }}</li>
+                        @endif
+                    </ul>
                     </p>
                     <div class="d-flex">
-                        <!-- <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" @if($producto->existencia === 0){ disabled } @endif/>
-                        <button class="btn btn-outline-dark flex-shrink-0 me-3" type="button" @if($producto->existencia === 0){ disabled } @endif>
-                            <i class="fa fa-cart-plus" aria-hidden="true"></i>
-                            Añadir al carrito
-                        </button> -->
+                        <!-- <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" @if ($producto->existencia === 0) { disabled } @endif/>
+                                            <button class="btn btn-outline-dark flex-shrink-0 me-3" type="button" @if ($producto->existencia === 0) { disabled } @endif>
+                                                <i class="fa fa-cart-plus" aria-hidden="true"></i>
+                                                Añadir al carrito
+                                            </button> -->
                         @can('productos.edit')
-                        <a href="/productos/{{$producto->id}}/edit" class="btn btn-primary flex-shrink-0">
-                            <i class="fas fa-fw fa-edit" aria-hidden="true"></i>
-                            Editar producto
-                        </a>
+                            <a href="/productos/{{ $producto->id }}/edit" class="btn btn-primary flex-shrink-0">
+                                <i class="fas fa-fw fa-edit" aria-hidden="true"></i>
+                                Editar producto
+                            </a>
                         @endcan
                     </div>
                 </div>
@@ -69,74 +74,74 @@
     </section>
     <!-- Sección de productos relacionados-->
     @php
-        $ruta1 = "img/product_generic_img_3.jpg";
-        $ruta2 = "storage/img/name";
-        $ruta_img = "";
+        $ruta1 = 'img/product_generic_img_3.jpg';
+        $ruta2 = 'storage/img/name';
+        $ruta_img = '';
     @endphp
     <section class="py-5 bg-light">
         <div class="container px-4 px-lg-5 mt-5">
             @if (!$relacionados->isEmpty())
-            <h2 class="fw-bolder mb-4">Productos parecidos</h2>
-            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">                
-                    <!-- Aqui comienza el foreach -->
-                    @foreach($relacionados as $producto)
-                    <div class="col mb-5">
-                        <div class="card h-100">
-                            <!-- Badge (opcional) (podria ser 'Disponible', 'Nuevo', 'agotado' o algo asi-->
-                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">                           
-                                @if ($producto->existencia == 0)
-                                    Agotado
-                                @elseif ($producto->existencia < 5)
-                                    Por agotarse
-                                @else
-                                    Disponible
-                                @endif
-                            </div>
-                            <!-- Imagen de producto -->
-                            <a class="nav-link " href="/detalle/producto/{{$producto->id}}">
-                                @php($ruta_img = $ruta1)
-                                @foreach ($imagenes_rel as $imagen)
-                                    @if(($imagen->id_registro) == ($producto->id))
-                                        @php($ruta_img = str_replace('name',$imagen->nombre_imagen,$ruta2))
-                                    @endif                   
-                                @endforeach    
-                                <img class="card-img-top" style="width: 100%; height: 136px;" src="{{ asset($ruta_img) }}" alt="Imagen de {{ $producto->nombre }} />
-                            </a>
-                            <!-- Detalle del producto-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Nombre del producto-->
-                                    <h5 class="fw-bolder">{{$producto->nombre}}</h5>
-                                    <!-- Product reviews (TAGS) (opcional)-->
-                                    <div class="d-flex justify-content-center small mb-2">
-                                        {{ $producto->categoria }} - {{ $producto->marca }} - {{ $producto->color }}
-                                        <!-- <div class="bi-star-fill">A</div>
-                                        <div class="bi-star-fill">B</div>
-                                        <div class="bi-star-fill">C</div>
-                                        <div class="bi-star-fill">D</div>
-                                        <div class="bi-star-fill">E</div> -->
-                                    </div>
-                                    <!-- Precio-->
-                                    <!-- <span class="text-muted text-decoration-line-through">$20.00</span> -->
-                                    {{ $producto->precio_venta }} Bs. / {{ $producto->unidad}}
+                <h2 class="fw-bolder mb-4">Productos parecidos</h2>
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    @foreach ($relacionados as $producto)
+                        <div class="col mb-5">
+                            <div class="card h-100">
+                                <!-- Badge (opcional) (podria ser 'Disponible', 'Nuevo', 'agotado' o algo asi-->
+                                <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">
+                                    @if ($producto->existencia == 0)
+                                        Agotado
+                                    @elseif ($producto->existencia < 5)
+                                        Por agotarse
+                                    @else
+                                        Disponible
+                                    @endif
                                 </div>
+                                <!-- Imagen de producto -->
+                                <a class="nav-link " href="/detalle/producto/{{ $producto->id }}">
+                                    @php($ruta_img = $ruta1)
+                                    @foreach ($imagenes_rel as $imagen)
+                                        @if ($imagen->id_registro == $producto->id)
+                                            @php($ruta_img = str_replace('name', $imagen->nombre_imagen, $ruta2))
+                                        @endif
+                                    @endforeach
+                                    <img class="card-img-top" style="width: 100%; height: 136px;"
+                                        src="{{ asset($ruta_img) }}" alt="Imagen de {{ $producto->nombre }}" />
+                                </a>
+                                <!-- Detalle del producto-->
+                                <div class="card-body
+                                        p-4">
+                                    <div class="text-center">
+                                        <!-- Nombre del producto-->
+                                        <h5 class="fw-bolder">{{ $producto->nombre }}</h5>
+                                        <!-- Product reviews (TAGS) (opcional)-->
+                                        <div class="d-flex justify-content-center small mb-2">
+                                            {{ $producto->categoria }} - {{ $producto->marca }} - {{ $producto->color }}
+                                            <!-- <div class="bi-star-fill">A</div>
+                                                            <div class="bi-star-fill">B</div>
+                                                            <div class="bi-star-fill">C</div>
+                                                            <div class="bi-star-fill">D</div>
+                                                            <div class="bi-star-fill">E</div> -->
+                                        </div>
+                                        <!-- Precio-->
+                                        <!-- <span class="text-muted text-decoration-line-through">$20.00</span> -->
+                                        {{ $producto->precio_venta }} Bs. / {{ $producto->unidad }}
+                                    </div>
+                                </div>
+                                <!-- Acciones-->
+                                <!-- <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Añadir al carrito</a></div>
+                                </div> -->
                             </div>
-                            <!-- Acciones-->
-                            <!-- <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Añadir al carrito</a></div>
-                            </div> -->
                         </div>
-                    </div>
                     @endforeach
-                    <!-- Aqui acaba el foreach -->
-            </div>
+                </div>
             @endif
         </div>
     </section>
 @stop
 
 @section('css')
-    
+
 @stop
 
 @section('js')
